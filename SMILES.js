377acceec526e50afd232e376575470accd1bb5f
Martin Bohun <martin.bohun@gmail.com>
2012-07-24 22:33:15 +1000

    added first impl of smiles_tokenize(), plus some minor cleanup

--- SMILES.js
+++ SMILES.js
@@ -1,8 +1,5 @@
 (function (glob) {
 
-    var dummy = 42,
-	x = 666;
-
     function smiles_init() {
 	glob.win.document.removeEventListener("DOMContentLoaded", smiles_init, false);
 	// this is only a demo, the real thing should scan the whole document
@@ -20,39 +17,76 @@
 	console.log("[smi]" + smi);
 
 	var tok = smiles_tokenize(smi);
-	var tok_dump = "";
-	for (var i = 0; i < tok.length; i++) {
-	    tok_dump = tok_dump + tok[i] + ","; 
-	}
-
-	console.log("[tok]" + tok_dump);
+	console.log("[tok]" + tok);
 
 	// var mat_adj = smiles_matrix_adjacency(tok);
-
-	smiles_render(c, smi);
-	//alert(smi);
+	smiles_render(c, tok);
     }
 
-    // TODO: not the real thing yet!
-    function smiles_tokenize(smi) {
-	var tok = [];
-	for (var i = 0; i < smi.length; i++) {
-	    tok.push(String(smi.charAt(i)));
-	}
+    var smiles_tokenize = (function () {
+        "use strict";
 
-	return tok;
-    }
+        var table = [
+            "(", ")", "=", "#", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "/", "\\",
+	    "[", "]", "@", ".", "+", "-", "%", "*"
+
+	].concat([
+            "H",                                                                                                  "He",
+	    "Li", "Be",                                                             "B",  "C",  "N",   "O", "F",  "Ne",
+	    "Na", "Mg",                                                             "Al", "Si", "P",   "S", "Cl", "Ar",
+	    "K",  "Ca", "Sc", "Ti", "V",  "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr",
+	    "Rb", "Sr", "Y",  "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I",  "Xe",
+            "Cs", "Ba", "Lu", "Hf", "Ta", "W",  "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn",
+	    "Fr", "Ra", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Rg", "Cn",
+	                      "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb",
+                              "Ac", "Th", "Pa", "U",  "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No"
+
+        ].sort().reverse().concat(["c", "n", "o", "p", "s"])),
+
+        match_symbol = function (smiles, offset, tokens) {
+            var i, symbol;
+            for (i = 0; i < table.length; i += 1) {
+                symbol = table[i];
+                if (symbol === smiles.substr(offset, symbol.length)) {
+                    tokens.push(symbol);
+                    return symbol.length;
+                }
+            }
+            return 0;
+        };
+
+	console.log("table:" + table);
+
+        return function (smiles) {
+            var tok = [],
+                i = 0,
+                match = 0;
+
+            while (i < smiles.length) {
+                match = match_symbol(smiles, i, tok);
+                if (match > 0) {
+                    i = i + match;
+
+                } else {
+                    console.log("smiles_tokenize error: no match[" + i + "]:"
+				+ smiles.substr(i, 1));
+                    i = i + 1;
+                }
+            }
+            return tok;
+        };
+    }());
 
     // TODO: only a demo
     function smiles_render(c, mat) {
-	ctx = c.getContext("2d");
+	var ctx = c.getContext("2d");
 
 	ctx.clearRect(0, 0, c.width, c.height);
 
 	ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
 	ctx.fillRect(0, 0, 320, 100);
 
-	ctx.font = "16pt DejaVu";
+	ctx.font = "10pt DejaVu";
 	ctx.fillText(mat, 5, 20);
     }
 
